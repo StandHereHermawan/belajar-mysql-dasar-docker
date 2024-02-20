2fa2b377dc52901f5044188500c85cda8ebd9734
Arief-Karditya <nolifegansaya@gmail.com>
2024-02-20 10:12:42 +0700

    Table, Select Distinct Ops

---
+++ mysql-notes/table/12-select-distinct-ops/script-in-ops.sql
@@ -0,0 +1,20 @@
+CREATE TABLE products
+(
+    id          VARCHAR(10)  NOT NULL,
+    name        VARCHAR(100) NOT NULL,
+    description TEXT,
+    category    ENUM ('Makanan', 'Minuman','Lain-Lain'),
+    price       INT UNSIGNED NOT NULL,
+    quantity    INT UNSIGNED NOT NULL DEFAULT 0,
+    created_at  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+) Engine = InnoDB;
+
+SELECT products.id, products.category
+FROM products;
+
+SELECT DISTINCT products.category FROM products;
+
+SELECT DISTINCT products.id,products.category FROM products;
+
+SELECT DISTINCT products.price,products.category FROM products;
